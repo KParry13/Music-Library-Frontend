e5cfe045e338d7c6bdd54ef5a51839a33d724d76
Kayla <k_meijer_10@yahoo.com>
2023-04-13 11:56:51 -0400

    completed cypress test on delete button

--- cypress/e2e/music.cy.js
+++ cypress/e2e/music.cy.js
@@ -1,12 +1,12 @@
 describe('Music Library Test', () => {
   it('passes', () => {
     cy.visit('http://localhost:3000/')
-    cy.get("[data-test='title']").type("Any Man Of Mine")
-    cy.get("[data-test='artist']").type("Shania Twain")
-    cy.get("[data-test='album']").type("Come On Over")
-    cy.get("[data-test='release_date']").type("2001-10-25")
-    cy.get("[data-test='genre']").type("Country")
-    cy.get("[data-test='running_time']").type(236)
+    cy.get("[data-test='title']").type("I Love Rock 'N Roll")
+    cy.get("[data-test='artist']").type("Joan Jett")
+    cy.get("[data-test='album']").type("Greatest Hits")
+    cy.get("[data-test='release_date']").type("1981-09-16")
+    cy.get("[data-test='genre']").type("Classic Rock")
+    cy.get("[data-test='running_time']").type(242)
     cy.get("[data-test='submit btn']").click()
 
   })
@@ -15,5 +15,14 @@ describe('Music Library Test', () => {
     cy.visit('http://localhost:3000/')
     cy.get("[data-test='input']").type('Blue')
     cy.should('have.value', 'Blue')
+  })
+
+  it('passes', () => {
+    cy.visit('http://localhost:3000/')
+    cy.contains('Any Man Of Mine')
+    .parents('tr')
+    .find('button')
+    .contains('Delete')
+    .click()
   })
 })
